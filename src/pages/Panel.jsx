969db6be28a868175c4177a6675daeef5d826e77
C4lumny <ospinonathan@gmail.com>
2024-02-05 15:41:05 -0500

    Create de asignaturas finalizado

--- src/pages/Panel.jsx
+++ src/pages/Panel.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Teachers } from "./administrators/teachers";
 import { Courses } from "./administrators/Courses";
+import { Subjects } from "./administrators/Subjects"
 import { AsideOption } from "../components/AsideOption";
 // Recursos 👇
 import default_user from "../assets/default_user.webp";
@@ -8,7 +9,7 @@ import default_user from "../assets/default_user.webp";
 export const Panel = () => {
   const [active, setActive] = useState({
     inicio: false,
-    materias: false,
+    asignaturas: false,
     cursos: false,
     profesores: false,
     estudiantes: false,
@@ -18,7 +19,7 @@ export const Panel = () => {
   const handleClick = (id) => {
     setActive({
       inicio: id === "inicio",
-      materias: id === "materias",
+      asignaturas: id === "asignaturas",
       cursos: id === "cursos",
       profesores: id === "profesores",
       estudiantes: id === "estudiantes",
@@ -62,11 +63,11 @@ export const Panel = () => {
                 </svg>
               </AsideOption>
               {/* 👇 OPCION DE MATERIAS */}
-              <AsideOption id="materias" active={active.materias} handleClick={handleClick} titulo="MATERIAS">
+              <AsideOption id="asignaturas" active={active.asignaturas} handleClick={handleClick} titulo="ASIGNATURAS">
                 <svg width="32px" height="32px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path
                     d="M4 19V6.2C4 5.0799 4 4.51984 4.21799 4.09202C4.40973 3.71569 4.71569 3.40973 5.09202 3.21799C5.51984 3 6.0799 3 7.2 3H16.8C17.9201 3 18.4802 3 18.908 3.21799C19.2843 3.40973 19.5903 3.71569 19.782 4.09202C20 4.51984 20 5.0799 20 6.2V17H6C4.89543 17 4 17.8954 4 19ZM4 19C4 20.1046 4.89543 21 6 21H20M9 7H15M9 11H15M19 17V21"
-                    stroke={`${active.materias ? "#27272a" : "#a1a1aa"}`}
+                    stroke={`${active.asignaturas ? "#27272a" : "#a1a1aa"}`}
                     stroke-width="1"
                     stroke-linecap="round"
                     stroke-linejoin="round"
@@ -167,6 +168,7 @@ export const Panel = () => {
         )}
         {active.profesores && <Teachers />}
         {active.cursos && <Courses />}
+        {active.asignaturas && <Subjects />}
       </main>
     </div>
   );
---
+++ src/pages/administrators/Subjects.jsx
@@ -0,0 +1,73 @@
+import { useState } from "react";
+import SidebarNav from "@/components/SidebarNav";
+import { Separator } from "@/components/ui/separator";
+import { ViewSubjects } from "@/components/subjects/view";
+import { CreateSubjects } from "@/components/subjects/Create";
+import { DeleteSubjects } from "@/components/subjects/Delete";
+import { UpdateSubjects } from "@/components/subjects/Update";
+
+export const Subjects = () => {
+  const [active, setActive] = useState({
+    visualizar: true,
+    crear: false,
+    borrar: false,
+    actualizar: false,
+  });
+
+  const handleClick = (id) => {
+    setActive({
+      visualizar: id === "visualizar",
+      crear: id === "crear",
+      borrar: id === "borrar",
+      actualizar: id === "actualizar",
+    });
+  };
+
+  const sidebarNavItems = [
+    {
+      title: "Visualizar materias",
+      onClick: () => handleClick("visualizar"),
+      isActive: active.visualizar,
+    },
+    {
+      title: "Crear materia",
+      onClick: () => handleClick("crear"),
+      isActive: active.crear,
+    },
+    {
+      title: "Borrar materia",
+      onClick: () => handleClick("borrar"),
+      isActive: active.borrar,
+    },
+    {
+      title: "Actualizar materia",
+      onClick: () => handleClick("actualizar"),
+      isActive: active.actualizar,
+    },
+  ];
+
+  return (
+    <div className="flex justify-center w-full min-h-screen h-full items-center overflow-hidden py-20">
+      <div className="hidden p-20 md:block bg-white w-4/5 border border-zinc-300 rounded-md shadow-md">
+        <div className="space-y-0.5">
+          <h2 className="text-2xl font-semibold tracking-tight">Materias</h2>
+          <p className="text-muted-foreground">¡Administra a las materias de tu institución!</p>
+        </div>
+        <Separator className="my-6" />
+        <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
+          <aside className="-mx-4 lg:w-1/5">
+            <SidebarNav items={sidebarNavItems} />
+          </aside>
+          <div className="flex-1 lg:max-w-4xl my-10">
+            <div className="space-y-6">
+              {active.visualizar && <ViewSubjects />}
+              {active.crear && <CreateSubjects />}
+              {active.borrar && <DeleteSubjects />}
+              {active.actualizar && <UpdateSubjects />}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
